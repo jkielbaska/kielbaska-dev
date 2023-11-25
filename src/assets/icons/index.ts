6f2dc9c45b0bad48f683f61cdd96f8bf2cefd349
Jakub <kielbaskakuba@gmail.com>
2023-11-25 02:04:05 +0100

    Add icons to display them on about section

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,55 @@
+import css from "./css.svg";
+import express from "./express.svg";
+import git from "./git.svg";
+import github from "./github.svg";
+import html from "./html.svg";
+import javascript from "./javascript.svg";
+import motion from "./motion.svg";
+import mui from "./mui.svg";
+import nextjs from "./nextjs.svg";
+import nodejs from "./nodejs.svg";
+import react from "./react.svg";
+import redux from "./redux.svg";
+import sass from "./sass.svg";
+import tailwindcss from "./tailwindcss.svg";
+import typescript from "./typescript.svg";
+import soundon from "./soundon.png";
+import soundoff from "./soundoff.png";
+import jest from "./jest.svg";
+import styledComponents from "./styled-components.svg";
+import cypress from "./cypress.svg";
+import three from "./three.svg";
+import mysql from "./mysql.svg";
+import prisma from "./prisma.svg";
+import firebase from "./firebase.svg";
+import hookForm from "./react-hook-form.svg";
+import reactQuery from "./react-query.svg";
+
+export {
+  css,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  react,
+  redux,
+  sass,
+  tailwindcss,
+  typescript,
+  soundon,
+  soundoff,
+  jest,
+  styledComponents,
+  cypress,
+  three,
+  mysql,
+  prisma,
+  firebase,
+  hookForm,
+  reactQuery,
+};
