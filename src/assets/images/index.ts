a53682ace3a8e0e9cf62298338637fb12d84ad7f
Jakub <kielbaskakuba@gmail.com>
2023-11-25 03:16:01 +0100

    Add images for all projects in About

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+import countrySearch from "./country-search.svg";
+import scrapeNews from "./scrape-news.svg";
+import starWarsInfo from "./star-wars-info.svg";
+import wurstcord from "./wurstcord.svg";
+
+export { countrySearch, scrapeNews, starWarsInfo, wurstcord };
