e13b833f2b3b4658b98be2595cdc221350b8f507
Jakub <kielbaskakuba@gmail.com>
2023-11-26 22:29:45 +0100

    Change loader styling for 3D models

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,8 +3,8 @@ import { Html } from "@react-three/drei";
 export const Loader = () => {
   return (
     <Html>
-      <div className="w-full h-full flex  justify-center items-center">
-        <div className="w-20 h-20 border-2 border-opacity-20 border-borderOrange border-t-borderOrange rounded-full animate-spin" />
+      <div className="absolute top-0 bottom-0 right-0 left-0 flex">
+        <div className="h-20 border-2 border-opacity-40 border-borderOrange animate-spin" />
       </div>
     </Html>
   );
